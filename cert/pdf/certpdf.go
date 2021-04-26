2697e4a298a3582d2e6e3fdab030e706bd6f2571
Marc Brillault <marc.brillault@manomano.com>
2021-04-26 18:44:11 +0200

    Added images + header generation

--- cert/pdf/certpdf.go
+++ cert/pdf/certpdf.go
@@ -32,6 +32,10 @@ func (p *PdfSaver) Save(cert cert.Cert) error {
 	pdf.SetTitle(cert.LabelTitle, true)
 	pdf.AddPage()
 
+	background(pdf)
+
+	header(pdf, &cert)
+
 	// save file
 	file := fmt.Sprintf("%v.pdf", cert.LabelTitle)
 	path := path.Join(p.OutputDir, file)
@@ -42,4 +46,32 @@ func (p *PdfSaver) Save(cert cert.Cert) error {
 
 	fmt.Printf("Certificate %v has been generated\n", file)
 	return nil
+}
+
+func background(pdf *gofpdf.Fpdf) {
+	opts := gofpdf.ImageOptions{
+		ImageType: "png",
+	}
+
+	width, height := pdf.GetPageSize()
+	pdf.ImageOptions("img/background.png", 0, 0, width, height, false, opts, 0, "")
+}
+
+func header(pdf *gofpdf.Fpdf, c *cert.Cert) {
+	opts := gofpdf.ImageOptions{
+		ImageType: "png",
+	}
+
+	margin := 30.0
+	marginH := 20.0
+	// x := 0.0
+	imageWidth := 30.0
+	pageWidth, _ := pdf.GetPageSize()
+	filename := "img/gopher.png"
+
+	pdf.ImageOptions(filename, margin, marginH, imageWidth, 0, false, opts, 0, "")
+	pdf.ImageOptions(filename, pageWidth-imageWidth-margin, marginH, imageWidth, 0, false, opts, 0, "")
+
+	pdf.SetFont("Helvetica", "", 40)
+	pdf.WriteAligned(0, 50, c.LabelCompletion, "C")
 }
