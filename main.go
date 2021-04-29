26707e8855fa3fa9237756908d71c9a16e24a5e6
Marc Brillault <marc.brillault@manomano.com>
2021-04-29 18:21:05 +0200

    Added cli flags to choose the generation type
    removed the generated binary from the repository

--- main.go
+++ main.go
@@ -1,24 +1,38 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
 	"example.com/go-cert/cert"
-	"example.com/go-cert/html"
+	"example.com/go-cert/cert/html"
+	"example.com/go-cert/cert/pdf"
 )
 
 func main() {
-	c, err := cert.New("Golang Programing", "Bob Dylan", "2018-06-21")
+	outputType := flag.String("type", "pdf", "Output type of the certificate")
+	flag.Parse()
+
+	var saver cert.Saver
+	var err error
+
+	switch *outputType {
+	case "html":
+		saver, err = html.New("output")
+	case "pdf":
+		saver, err = pdf.New("output")
+	default:
+		err = fmt.Errorf("Unknown output type, got '%v'", *outputType)
+	}
 	if err != nil {
-		fmt.Printf("Error during certificate creation: %v", err)
+		fmt.Printf("Could not create generator: %v\n", err)
 		os.Exit(1)
 	}
 
-	var saver cert.Saver
-	saver, err = html.New("output")
+	c, err := cert.New("Golang Programing", "Bob Dylan", "2018-06-21")
 	if err != nil {
-		fmt.Printf("Error during pdf generation: %v\n", err)
+		fmt.Printf("Error during certificate creation: %v", err)
 		os.Exit(1)
 	}
 
